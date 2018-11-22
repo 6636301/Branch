4f83007d7b3a49d71aec6119391459c5e21611f6
Alex Ivanov <6636301@gmail.com>
2018-11-22 12:56:59 +0300

    Cycle_if

---
+++ PythonApplication_Example1/Cycles.py
@@ -0,0 +1,6 @@
+x =int(input("Press A: "))
+if x > 5: 
+    print("This nimber is greater then five")
+
+if number is not None:
+    pass #TODO: add some logic here later
---
+++ PythonApplication_Example1/Math.py
@@ -0,0 +1,20 @@
+import math
+
+x = - 3.265
+
+print('x =' ,x)
+print('tranck(x) =',math.trunc(x))
+print('floor(x) =',math.floor(x))
+print('ceil(x) =',math.ceil(x))
+
+print(math.pi)
+print(math.e)
+
+print(math.sin(math.pi/4))
+
+
+print(1/math.sqrt(2))
+
+
+-2<x<3
+
