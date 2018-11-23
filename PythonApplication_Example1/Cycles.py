a08bf6db68661e858874ca5181c72596e9ce7d80
Alex Ivanov <6636301@gmail.com>
2018-11-23 11:39:20 +0300

    Cycle if_modificated_elif

--- PythonApplication_Example1/Cycles.py
+++ PythonApplication_Example1/Cycles.py
@@ -1,12 +1,11 @@
-x = float(input("Enter number2: "))
+x = float(input("Enter number: "))
 
-if 0 < x < 7:
-        print("Value in range, let's continue")
-        y = 2 * x - 5
-        if (y > 0):
-            print("y is possitive")
-        else:
-            if (y < 0):
-                print("y is negative")
-            else:
-                print("y is 0")
+if (0 < x < 7):
+    print("It's normal.Continiue...")
+    y = 2 * x - 5
+    if (y > 0):
+        print("y greater then 0")
+    elif (y < 0):
+        print("y smaller then 0")
+    else:
+         print("y is 0")
